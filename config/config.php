0e3e47f3a4adec17ed58bcbdcdaab65cf642734e
majidmohammadian <majeedmohammadian@gmail.com>
2025-07-16 11:47:13 +0330

    added config file

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'role' => 'roles',
+        'role_path' => 'role_paths',
+        'membership' => 'memberships',
+        'representative' => 'representatives',
+        'role_rule' => 'role_rules',
+        'role_activity_log' => 'role_activity_logs',
+    ],
+
+];
