3e48e6146e317c56d1504509f616ddd6273c1386
majidmohammadian <majeedmohammadian@gmail.com>
2025-07-18 23:03:09 +0330

    added comment to RepresentativeStatusEnum

--- src/Enums/RepresentativeStatusEnum.php
+++ src/Enums/RepresentativeStatusEnum.php
@@ -5,6 +5,8 @@ namespace JobMetric\Rolix\Enums;
 use JobMetric\PackageCore\Enums\EnumToArray;
 
 /**
+ * Enum representing the various statuses of a representative delegation request.
+ *
  * @method static PENDING()
  * @method static ACTIVE()
  * @method static CANCEL()
@@ -15,9 +17,18 @@ enum RepresentativeStatusEnum: string
 {
     use EnumToArray;
 
+    /** The delegation request is pending and awaiting action */
     case PENDING = "pending";
+
+    /** The delegation is currently active and valid */
     case ACTIVE = "active";
+
+    /** The delegation has been canceled */
     case CANCEL = "cancel";
+
+    /** The delegation request has been rejected */
     case REJECT = "reject";
+
+    /** The delegation has expired due to passing its valid time */
     case EXPIRE = "expire";
 }
